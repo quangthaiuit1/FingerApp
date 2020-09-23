8b0487998955e9808e2d41c5e4b6b58bc5c2fa99
QuangThai <quangthaiuit1@gmail.com>
2020-09-23 08:35:54 +0700

    commit source

---
+++ src/lixco/com/entity/FoodNhaAn.java
@@ -0,0 +1,25 @@
+package lixco.com.entity;
+
+public class FoodNhaAn {
+	private String employeeName;
+	private  byte[] imageFood;
+	private String employeeIdOld;
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public byte[] getImageFood() {
+		return imageFood;
+	}
+	public void setImageFood(byte[] imageFood) {
+		this.imageFood = imageFood;
+	}
+	public String getEmployeeIdOld() {
+		return employeeIdOld;
+	}
+	public void setEmployeeIdOld(String employeeIdOld) {
+		this.employeeIdOld = employeeIdOld;
+	}
+}	
